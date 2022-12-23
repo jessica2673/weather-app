b095c9cbd7141ed14852c46ee2b9fe4116babf4e
Jessica <jessica2003mail@gmail.com>
2022-12-22 20:51:48 -0500

    Added a Fahrenheit to degrees celsius toggle.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const form = document.getElementById('form');
 const moreInfo = document.getElementById('moreInfo');
 const errorMssg = document.getElementById('error');
+const toggleBtn = document.getElementById('toggle');
+const unit = ["F", "°C"];
+let selector = 0; //where 0 represents Fahrenheit
+let storedCity = "Toronto";
 getData("Toronto");
+
+toggleBtn.addEventListener("click", e => {
+    selector = Math.abs(1 - selector);
+
+    if (storedCity != "") {
+        getData(storedCity);
+    } else {
+        errorMssg.textContent = "Please enter a valid location first.";
+    }
+});
 
 form.addEventListener("submit", e => {
     e.preventDefault();
+    storedCity = document.querySelector('input[name=search]').value;
 
-    if(document.querySelector('input[name=search]').value != "") {
-        getData(document.querySelector('input[name=search]').value).catch(function (err) {
+    if(storedCity != "") {
+        getData(storedCity).catch(function (err) {
             console.log(err);
             throw new Error();
           });
@@ -55,13 +70,22 @@ async function printData(data) {
         city.textContent = data['name'];
         date.textContent = `${getDate.getDay()} ${months[getDate.getMonth()]} ${getDate.getFullYear()}, ${getDate.getHours()}:${getDate.getMinutes()}`;
         icon.src=`http://openweathermap.org/img/wn/${data['weather'][0].icon}@2x.png`;
-        temp.textContent = `${data['main'].temp}F`;
+        
         desc.textContent = data['weather'][0].description;
 
-        card.appendChild(createInfo("Feels like: ", data['main'].feels_like, 'moreInfo'));
-        card.appendChild(createInfo("Min temp: ", data['main'].temp_min, 'moreInfoSmall'));
-        card.appendChild(createInfo("Max temp: ", data['main'].temp_max, 'moreInfoSmall'));
-        card.appendChild(createInfo("Pressure: ", data['main'].pressure, 'moreInfo'));
+        if (selector === 0) {
+            temp.textContent = `${data['main'].temp}${unit[selector]}`;
+            card.appendChild(createInfo("Feels like: ", `${data['main'].feels_like}${unit[selector]}`, 'moreInfo'));
+            card.appendChild(createInfo("Min temp: ", `${data['main'].temp_min}${unit[selector]}`, 'moreInfoSmall'));
+            card.appendChild(createInfo("Max temp: ", `${data['main'].temp_max}${unit[selector]}`, 'moreInfoSmall'));
+        } else {
+            temp.textContent = `${fToC(data['main'].temp)}${unit[selector]}`;
+            card.appendChild(createInfo("Feels like: ",`${fToC(data['main'].feels_like)}${unit[selector]}`, 'moreInfo'));
+            card.appendChild(createInfo("Min temp: ", `${fToC(data['main'].temp_min)}${unit[selector]}`, 'moreInfoSmall'));
+            card.appendChild(createInfo("Max temp: ", `${fToC(data['main'].temp_max)}${unit[selector]}`, 'moreInfoSmall'));
+        }
+
+        card.appendChild(createInfo("Pressure: ", `${data['main'].pressure}Hg`, 'moreInfo'));
         card.appendChild(createInfo("Humidity: ", data['main'].humidity, 'moreInfo'));
 
         moreInfo.appendChild(card);
@@ -86,6 +110,10 @@ function removeCard() {
     if (card != null) {
         card.parentNode.removeChild(card);
     }
+}
+
+function fToC(temp) {
+    return ((5*(temp - 32))/9).toFixed(2);
 }
 
 
